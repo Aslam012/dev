99ca780a5427967b14d0d7da849a68c5dd19bcd7
shoaib <shoaib.shaikh@neoquant.com>
2022-12-23 17:58:14 +0530

    some db changes

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 
 exports.initialize_mongo_connection = async () => {
     try {
-        let mongoDB = "mongodb+srv://admin:admin@cluster0.fdqlf.mongodb.net/?retryWrites=true&w=majority";
+        // let mongoDB = "mongodb+srv://admin:admin@cluster0.fdqlf.mongodb.net/?retryWrites=true&w=majority";
+        let mongoDB =  "mongodb+srv://shoaib:shoaib@deviil.6pvkh.mongodb.net/?retryWrites=true&w=majority"
         await mongoose.connect(mongoDB);
         console.log("MongoDb Connected Successfully");
     } catch (error) {
+        console.log(error);
         console.log('Retrying to connect to Mongodb')
         setTimeout(data => {
             this.initialize_mongo_connection()
